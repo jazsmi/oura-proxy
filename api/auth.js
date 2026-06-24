5bd023b56bb35a1fbd5311d32473285ffc1baab6
jazsmi <54365670+jazsmi@users.noreply.github.com>
2026-06-24 11:43:52 -0400

    Handle production environment response in auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV === 'production') {
+  return res.status(404).end();
+}
 export default function handler(req, res) {
   const clientId = process.env.OURA_CLIENT_ID;
   const redirectUri = process.env.OURA_REDIRECT_URI;
